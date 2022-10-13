87cdc13da622a8209374399a79002f2f431d88f4
Alejandro <realpealejo@gmail.com>
2022-10-12 21:10:53 -0500

    simple post service

---
+++ posts/index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const bodyParse = require('body-parser')
+const { randomBytes } = require('crypto')
+
+const app = express()
+app.use(bodyParse.json())
+
+//mock data
+const posts = {};
+
+app.get('/posts', (req, res) => {
+    return res.send(posts)
+})
+
+app.post('/posts', (req, res) => {
+    const id = randomBytes(4).toString('hex')
+    const { title } = req.body
+
+    posts[id] = {
+        id,
+        title
+    }
+    return res.status(201).send(posts[id])
+})
+
+app.listen(4000, () => console.log("listening on 4000"))
