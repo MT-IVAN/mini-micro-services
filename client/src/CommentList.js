7d7aa4742c6aa0bfc15a3887948d8880c863eca2
MT-IVAN <ivanandreseraso@gmail.com>
2023-08-12 17:43:11 -0500

    conterize Client

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -1,22 +1,18 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import React from 'react'
 
-const CommentList = ({ postId }) => {
-    const [comments, setComments] = useState([])
-
-    const fetchData = async () => {
-    
-        const res = await axios.get(`http://localhost:4001/posts/${postId}/comments`)
-        setComments(res.data)
-        
-    }
-
-    useEffect(() => {
-        fetchData()
-    },[])
+const CommentList = ({ comments }) => {
 
     const renderedComments = comments?.map((comment) => {
-        return <li id={comment.id}>{comment.content}</li>
+        let content
+
+        if (comment.status === 'approved') {
+            content = comment.content
+        } else if (comment.status === 'pending') {
+            content = 'This comment is waiting moderation'
+        } else if (comment.status === 'rejected') {
+            content = 'This comment has been rejected'
+        }
+        return <li key={comment.id} id={comment.id}>{content}</li>
     })
 
     return <ul>
